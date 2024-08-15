7d6ff182bdeb7a638d31e53ccabb4323bc043930
yaronmir <38312178+yaronmir@users.noreply.github.com>
2024-08-15 11:43:00 +0300

    Adding xmlCheck

---
+++ appengine/src/FileCheck/CheckUserInputForFiles.java
@@ -0,0 +1,26 @@
+package FileCheck;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class CheckUserInputForFiles extends CheckForXMLFile {
+     protected  File checkFileUserInput() {
+        String filePath;
+        Scanner scanner = new Scanner(System.in);
+        File fileToCheck;
+
+        do {
+            System.out.print("Enter the file path: ");
+            filePath = scanner.nextLine();
+            fileToCheck = getXMLFile(filePath);
+
+            if (fileToCheck == null) {
+                System.out.println("Invalid file. Ensure it exists and is an XML file. Please try again.");
+            }
+        } while (fileToCheck == null);
+
+        scanner.close();
+        return fileToCheck;
+    }
+
+}
