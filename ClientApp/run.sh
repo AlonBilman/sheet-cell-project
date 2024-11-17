d784d91623eefa7264e0285237527361edf2108d
Alon Bilman <bilman.alon@gmail.com>
2024-11-17 14:00:17 +0200

    run.sh, run.bat , war and jars are all up-to-date. edit .gitignore

--- ClientApp/run.sh
+++ ClientApp/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVAFX_LIB_PATH="../ThirdParty/JAVAFX2/openjfx-22.0.2_windows-x64_bin-sdk/javafx-sdk-22.0.2/lib/"
+JAVAFX_LIB_PATH="../ThirdParty/JAVAFX/openjfx-22.0.2_windows-x64_bin-sdk/javafx-sdk-22.0.2/lib"
 
 if [ ! -d "$JAVAFX_LIB_PATH" ]; then
     echo "Error: JavaFX library path not found at $JAVAFX_LIB_PATH"
